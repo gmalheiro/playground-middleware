6d65b197439b875d77d7d42136eb2e7c5be6f02b
gmalheiro <gabrielsilvamalheiro7@gmail.com>
2025-03-31 19:15:00 -0300

    feat: implement JSON validation function with structured error responses

---
+++ pkg/web/request/request_validate.go
@@ -0,0 +1,36 @@
+package request
+
+import (
+	"errors"
+
+	"github.com/gmalheiro/playground-middleware/pkg/web/response"
+	"github.com/go-playground/validator/v10"
+)
+
+var validate = validator.New(validator.WithRequiredStructEnabled())
+
+func validateJSON(object any) []response.Validation {
+	if err := validate.Struct(object); err != nil {
+		var validateErrs validator.ValidationErrors
+		fieldErrors := make(map[string][]string)
+
+		if errors.As(err, &validateErrs) {
+			for _, e := range validateErrs {
+				fieldName := e.Field()
+				fieldError := fieldName + " " + e.Tag()
+
+				fieldErrors[fieldName] = append(fieldErrors[fieldName], fieldError)
+			}
+		}
+
+		validates := make([]response.Validation, 0, len(fieldErrors))
+
+		for k, v := range fieldErrors {
+			validates = append(validates, response.Validation{Field: k, Messages: v})
+		}
+
+		return validates
+	}
+
+	return nil
+}
