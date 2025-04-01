2d7786209e4faefe088fcb83c1da77115854fabf
gmalheiro <gabrielsilvamalheiro7@gmail.com>
2025-04-01 13:44:47 -0300

    refactor: comment out unused methods in ProductRepository interface

--- internal/domain/contract/product.go
+++ internal/domain/contract/product.go
@@ -3,10 +3,11 @@ package contract
 import "github.com/gmalheiro/playground-golang-clean-arch/internal/domain/entity"
 
 type ProductRepository interface {
-	GetAll() ([]entity.Product, error)
-	GetByID(id int) (*entity.Product, error)
-	GetByCodeValue(codeValue string) (*entity.Product, error)
+	//TODO: implement commented methods
+	// GetAll() ([]entity.Product, error)
+	// GetByID(id int) (*entity.Product, error)
+	// GetByCodeValue(codeValue string) (*entity.Product, error)
 	Create(product entity.Product) (*entity.Product, error)
-	Update(product entity.Product) error
-	Delete(id int) error
+	// Update(product entity.Product) error
+	// Delete(id int) error
 }
