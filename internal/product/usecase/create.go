4b24e7c398e8aec932fd42c98309793f96f1ad64
gmalheiro <gabrielsilvamalheiro7@gmail.com>
2025-04-01 11:28:47 -0300

    feat: implement CreateProduct use case for product creation logic

---
+++ internal/product/usecase/create.go
@@ -0,0 +1,48 @@
+package usecase
+
+import (
+	"time"
+
+	"github.com/gmalheiro/playground-golang-clean-arch/internal/domain/contract"
+	"github.com/gmalheiro/playground-golang-clean-arch/internal/domain/entity"
+	"github.com/gmalheiro/playground-golang-clean-arch/internal/product/converter"
+	"github.com/gmalheiro/playground-golang-clean-arch/internal/product/dto"
+	product_error "github.com/gmalheiro/playground-golang-clean-arch/internal/product/error"
+	"github.com/gmalheiro/playground-golang-clean-arch/pkg/rest_err"
+)
+
+type CreateProductUseCase struct {
+	ProductRepository contract.ProductRepository
+}
+
+func NewCreateProductUseCase(repository contract.ProductRepository) *CreateProductUseCase {
+	return &CreateProductUseCase{
+		ProductRepository: repository,
+	}
+}
+
+func (puc *CreateProductUseCase) Execute(input dto.CreateProductDto) (*dto.ProductOutputDto, *rest_err.RestErr) {
+	product, err := puc.ProductRepository.GetByCodeValue(input.CodeValue)
+	if err != nil {
+		return nil, rest_err.NewInternalError(err.Error())
+	}
+	if product != nil {
+		return nil, rest_err.NewConflict(product_error.ErrConflictProductCodeValueAlreadyExist)
+	}
+	parsedDate, err := parseDate(input.Expiration)
+	if err != nil {
+		return nil, rest_err.NewInternalError(err.Error())
+	}
+	productEntity := entity.NewProduct(input.Name, input.Quantity, input.CodeValue, input.IsPublished, parsedDate, input.Price)
+	product, err = puc.ProductRepository.Create(*productEntity)
+	if err != nil {
+		return nil, rest_err.NewInternalError(err.Error())
+	}
+	output := converter.ToDTO(*product)
+	return &output, nil
+}
+
+func parseDate(dateStr string) (time.Time, error) {
+	layout := "02-01-2006"
+	return time.Parse(layout, dateStr)
+}
