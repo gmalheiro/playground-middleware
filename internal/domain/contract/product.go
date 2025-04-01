80251f36c2524d14ce0df7fdce1951f045739a0a
gmalheiro <gabrielsilvamalheiro7@gmail.com>
2025-04-01 11:16:09 -0300

    feat: add GetByCodeValue method to ProductRepository interface

--- internal/domain/contract/product.go
+++ internal/domain/contract/product.go
@@ -5,6 +5,7 @@ import "github.com/gmalheiro/playground-golang-clean-arch/internal/domain/entity
 type ProductRepository interface {
 	GetAll() ([]entity.Product, error)
 	GetByID(id int) (*entity.Product, error)
+	GetByCodeValue(codeValue string) (*entity.Product, error)
 	Create(product entity.Product) (*entity.Product, error)
 	Update(product entity.Product) error
 	Delete(id int) error
