7f8283e2bdc2e814ddfd9e1d7c3df5153ff19935
gmalheiro <gabrielsilvamalheiro7@gmail.com>
2025-04-02 16:30:30 -0300

    refactor: add http server, handler, controller and repository

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"github.com/gmalheiro/playground-golang-clean-arch/configs"
+	"github.com/gmalheiro/playground-golang-clean-arch/internal/infra/controller"
+	"github.com/gmalheiro/playground-golang-clean-arch/internal/infra/handler"
+	"github.com/gmalheiro/playground-golang-clean-arch/internal/infra/http"
+	"github.com/gmalheiro/playground-golang-clean-arch/internal/infra/repository"
+)
 
+func main() {
+	cfg := configs.NewConfig(".env.development")
+
+	hs := http.NewHttpServer(cfg.Server.Port)
+	productRepository := repository.NewProductMemoryRepository()
+
+	productHandler := handler.NewProductHandler(productRepository)
+
+	hs.RegisterController(controller.NewProductController(productHandler))
+
+	hs.Run()
 }
