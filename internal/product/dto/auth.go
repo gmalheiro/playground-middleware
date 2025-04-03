4f1e4ca06b3356d3cf9443986c804b14ad945101
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-04-03 10:57:31 -0300

    refactor: add auth output

--- internal/product/dto/auth.go
+++ internal/product/dto/auth.go
@@ -4,3 +4,7 @@ type AuthDto struct {
 	Email    string `json:"email,omitempty" validate:"required,email"`
 	Password string `json:"password,omitempty"`
 }
+
+type AuthOutputDto struct {
+	Token string `json:"token,omitempty"`
+}
