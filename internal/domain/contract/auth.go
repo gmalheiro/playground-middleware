a8a700868030a286ec2716d21a2596eb7a5e10f2
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-04-03 15:01:58 -0300

    refactor: add return type to be dto and rest err

--- internal/domain/contract/auth.go
+++ internal/domain/contract/auth.go
@@ -1,8 +1,12 @@
 package contract
 
-import "github.com/gmalheiro/playground-golang-clean-arch/internal/domain/entity"
+import (
+	"github.com/gmalheiro/playground-golang-clean-arch/internal/domain/entity"
+	"github.com/gmalheiro/playground-golang-clean-arch/internal/product/dto"
+	"github.com/gmalheiro/playground-golang-clean-arch/pkg/rest_err"
+)
 
 type AuthService interface {
-	Login(credentials entity.AuthCredentials) bool
+	Login(credentials entity.AuthCredentials) (*dto.AuthOutputDto, *rest_err.RestErr)
 	RefreshToken(token string)
 }
